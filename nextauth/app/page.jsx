80d4d5e08ddf1cdee1aad52e2e7c02419e029084
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2024-02-03 02:37:22 +0200

    protected  the pages

--- nextauth/app/page.jsx
+++ nextauth/app/page.jsx
@@ -1,8 +1,17 @@
 // "use client"
 
-import Login from "./components/login"
+import Login from "./components/login";
+import {authOptions} from "./api/auth/[...nextauth]/route"
 
-export default function Home() {
+//so no we want that when we are logged in, we should not access pages like the home login or register page OR type the url and then we access different pages, we want a more closed flow
+import {getServerSession} from "next-auth";
+import {redirect } from"next/navigation";
+
+export default async function Home() {
+
+  const session = await getServerSession(authOptions);
+
+    if (session) redirect("/Dashboard");
   return (
        <main>
         <Login/>
--- nextauth/app/register/page.jsx
+++ nextauth/app/register/page.jsx
@@ -1,6 +1,14 @@
 import RegisterForm from "../components/registerForm"
 import Link from "next/link"
+import {authOptions} from "../api/auth/[...nextauth]/route"
 
-export default function Register() {
+//so no we want that when we are logged in, we should not access pages like the home login or register page OR type the url and then we access different pages, we want a more closed flow
+import {getServerSession} from "next-auth";
+import {redirect } from"next/navigation";
+
+export default async function Register() {
+    const session = await getServerSession(authOptions);
+
+    if (session) redirect("/Dashboard");
     return (<RegisterForm/>);
 }
