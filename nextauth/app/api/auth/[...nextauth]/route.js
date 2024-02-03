bfad2c5a7efce5ac3ffa8592b6c58fe0d97e8ab3
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2024-02-03 02:13:12 +0200

    able to sign in because we have data in database,

--- nextauth/app/api/auth/[...nextauth]/route.js
+++ nextauth/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
+import {connectMongoDB} from "@/lib/mongodb";
+import User from "@/models/user";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
 
 const authOptions = {
     providers: [
@@ -9,8 +12,25 @@ const authOptions = {
             //  this can be filled to state that the passsword is a string or name is a string but since we areusing a custom login we wont
 
             async authorize(credentials) {
-                const user = {id: "1"};
-                return user;
+                const {email,password} = credentials;
+
+                try {
+                    await connectMongoDB();
+                    const user = await User.findOne({email});
+
+                    if(!user) {
+                        return null
+                    }
+                    const passwordsMatch = await bcrypt.compare(password, user.password);
+
+                    if(!passwordsMatch) {
+                        return null;
+                    }
+                    //if the passwords match that in the database then we want to return user
+                    return user;
+                }catch (error) {
+
+                }
             }
         })
 
